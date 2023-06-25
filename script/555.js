4b8765418a776c94d774f4d197121f46cd88b274
fqw000 <wang_zi_mo@163.com>
2023-06-25 09:14:20 +0800

    Update 555.js
    Signed-off-by: fqw000 <wang_zi_mo@163.com>

--- script/555.js
+++ script/555.js
@@ -1,9 +1,23 @@
 var body = $response.body;
 var obj = JSON.parse(body);
+
+// 屏蔽layout值为advert_self的项
 if (obj.data && obj.data.length > 0) {
-    var carousel = obj.data.find(item => item.title === '轮播图');
-    if (carousel && carousel.list && carousel.list.length > 0) {
-        carousel.list = carousel.list.filter(item => item.type !== 3);
+  var newData = [];
+  for (var i = 0; i < obj.data.length; i++) {
+    if (obj.data[i].layout !== "advert_self") {
+      newData.push(obj.data[i]);
     }
+  }
+  obj.data = newData;
 }
+
+// 过滤轮播图中type为3的项
+if (obj.data && obj.data.length > 0) {
+  var carousel = obj.data.find(item => item.title === '轮播图');
+  if (carousel && carousel.list && carousel.list.length > 0) {
+    carousel.list = carousel.list.filter(item => item.type !== 3);
+  }
+}
+
 $done({body: JSON.stringify(obj)});
