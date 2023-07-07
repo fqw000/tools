ae07a2cfc38e3fb21b2b709b6430f728f912da6a
fqw000 <wang_zi_mo@163.com>
2023-07-07 23:07:52 +0800

    Update 555.js
    Signed-off-by: fqw000 <wang_zi_mo@163.com>

--- script/555.js
+++ script/555.js
@@ -21,21 +21,3 @@ if (obj.data && obj.data.length > 0) {
 }
 
 $done({body: JSON.stringify(obj)});
-*/
-
-var body = $response.body;
-var obj = JSON.parse(body);
-
-if (obj.data && obj.data.length > 0) {
-  var newData = [];
-  var carousel = obj.data.find(item => item.title === '轮播图');
-  for (var i = 0; i < obj.data.length; i++) {
-    if (obj.data[i].layout !== "advert_self") {
-      newData.push(obj.data[i]);
-    }
-  }
-  if (carousel && carousel.list && carousel.list.length > 0) {
-    carousel.list = carousel.list.filter(item => item.type !== 3);
-  }
-  obj.data = newData;
-}
