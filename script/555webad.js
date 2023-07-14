1e92365d5b24b306b1b675d79fcda18a805d6b72
fqw000 <wang_zi_mo@163.com>
2023-07-14 18:14:10 +0800

    Update 555webad.js
    Signed-off-by: fqw000 <wang_zi_mo@163.com>

--- script/555webad.js
+++ script/555webad.js
@@ -7,47 +7,47 @@
 // # 555dianying.cc去广告脚本
 // http-response ^https?://www\.555dianying\.cc/ script-path=https://raw.githubusercontent.com/fqw000/tools/main/script/555webad.js, requires-body=true, timeout=10, tag=555web去广告
 
-    let body = $response.body;
-    let html = new TextDecoder().decode(body);
-    let parser = new DOMParser();
-    let doc = parser.parseFromString(html, 'text/html');
+let body = $response.body;
+let html = Buffer.from(body).toString();
+let parser = new DOMParser();
+let doc = parser.parseFromString(html, 'text/html');
   
-    // 获取所有class为"links"的A标签，并隐藏目标链接
-    var hideLinks = function() {
-      const links = doc.querySelectorAll('a.links');
-      links.forEach(link => {
-        if (link.getAttribute('href') === 'https://www.555hd4.com/vodtype/124.html') {
-          link.style.display = 'none';
-        }
-      });
+// 获取所有class为"links"的A标签，并隐藏目标链接
+var hideLinks = function() {
+  const links = doc.querySelectorAll('a.links');
+  links.forEach(link => {
+    if (link.getAttribute('href') === 'https://www.555hd4.com/vodtype/124.html') {
+      link.style.display = 'none';
     }
-  
-    // 屏蔽class值为"is_pc"和"is_mb"的div元素
-    var blockElements = function() {
-      var elements = doc.querySelectorAll('div.is_pc, div.is_mb');
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        element.parentNode.removeChild(element);
-      }
+  });
+}
+
+// 屏蔽class值为"is_pc"和"is_mb"的div元素
+var blockElements = function() {
+  var elements = doc.querySelectorAll('div.is_pc, div.is_mb');
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    element.parentNode.removeChild(element);
+  }
+}
+
+// 屏蔽包含有午夜福利字样的li元素
+var blockLiElements = function() {
+  var liElements = doc.querySelectorAll('li.swiper-slide.navbar-item');
+  for (var i = 0; i < liElements.length; i++) {
+    var liElement = liElements[i];
+    var spanElement = liElement.querySelector('span');
+    if (spanElement && spanElement.innerText === '午夜福利') {
+      liElement.parentNode.removeChild(liElement);
     }
-  
-    // 屏蔽包含有午夜福利字样的li元素
-    var blockLiElements = function() {
-      var liElements = doc.querySelectorAll('li.swiper-slide.navbar-item');
-      for (var i = 0; i < liElements.length; i++) {
-        var liElement = liElements[i];
-        var spanElement = liElement.querySelector('span');
-        if (spanElement && spanElement.innerText === '午夜福利') {
-          liElement.parentNode.removeChild(liElement);
-        }
-      }
-    }
-    
-    // 执行脚本
-    hideLinks();
-    blockElements();
-    blockLiElements();
-  
-    // 转换回字符串并返回修改后的响应内容
-    body = new TextEncoder().encode(doc.documentElement.outerHTML);
-    $done({body});
+  }
+}
+
+// 执行脚本
+hideLinks();
+blockElements();
+blockLiElements();
+
+// 转换回字符串并返回修改后的响应内容
+body = Buffer.from(doc.documentElement.outerHTML);
+$done({body});
