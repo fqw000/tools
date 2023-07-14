fcdb2eb636cebf3d4e07df30e323e096c42533d9
fqw000 <wang_zi_mo@163.com>
2023-07-14 17:45:39 +0800

    Update 555webad.js
    Signed-off-by: fqw000 <wang_zi_mo@163.com>

--- script/555webad.js
+++ script/555webad.js
@@ -8,19 +8,28 @@
 // http-response ^https?://www\.555dianying\.cc/ script-path=https://raw.githubusercontent.com/fqw000/tools/main/script/555webad.js, requires-body=true, timeout=10, tag=555web去广告
 
 
-var blockElements = function(response) {
-  var contentType = response.headers["Content-Type"];
-  if (contentType && contentType.indexOf("text/html") !== -1) {
-    var body = response.body;
-    var html = body.toString();
-    var doc = new DOMParser().parseFromString(html, "text/html");
-    var elements = doc.querySelectorAll('[id^="64b110e"]');
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      element.parentNode.removeChild(element);
-    }
-    response.body = doc.documentElement.outerHTML;
-  }
-}
+// 屏蔽555电影网广告和某个链接
+const modifyResponse = (body) => {
+  // 将响应体解析为JSON对象
+  const obj = JSON.parse(body);
 
-$done({ onResponse: blockElements });
+  // 屏蔽id以"64b110e"开头的div元素
+  if (obj.data && obj.data.length > 0) {
+    obj.data = obj.data.filter(item => {
+      return item.id && !item.id.startsWith('64b110e');
+    });
+  }
+
+  // 隐藏特定链接
+  const links = obj.links || [];
+  links.forEach(link => {
+    if (link.url === 'https://www.555hd4.com/vodtype/124.html') {
+      link.display = false;
+    }
+  });
+
+  // 将修改后的JSON对象序列化为字符串并返回
+  return JSON.stringify(obj);
+};
+
+$done({ body: modifyResponse($response.body) });
