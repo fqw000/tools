2074e18f98c90e556ba01e6a4d39f3550132d612
Zephyr <wang_zi_mo@163.com>
2024-09-16 16:41:53 +0800

    Update littlefox.js
    Signed-off-by: Zephyr <wang_zi_mo@163.com>

--- script/littlefox.js
+++ script/littlefox.js
@@ -2,7 +2,7 @@
 littlefox
 
 [rewrite_local]
-^https?:\/\/cdn\.littlefox\.co\.kr\/contents_5\/hls\/720\/.+\/stream\.m3u8\?_=\d+ url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/LittleFox.js
+^https?:\/\/cdn\.littlefox\.co\.kr\/contents_5\/hls\/720\/.+\/stream\.m3u8\?_=\d+ url script-request-header https://raw.githubusercontent.com/fqw000/tools/main/script/littlefox.js
 [mitm]
 hostname = cdn.littlefox.co.kr
 
